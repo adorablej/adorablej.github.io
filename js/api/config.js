effd9d5542c40109dd486d0cd46ebd95aebab452
adorablej <ekgo55@naver.com>
2026-08-20 15:10:14 +0900

    ..

--- js/api/config.js
+++ js/api/config.js
@@ -2,8 +2,8 @@
     'use strict';
 
     window.HunterAPIConfig = Object.freeze({
-        // 개발계 Swagger: http://115.68.192.132:8080/swagger-ui/index.html
-        baseUrl: 'http://115.68.192.132:8080',
+        // 개발계 Swagger: https://api-dev.hunterkorea.com/swagger-ui/index.html
+        baseUrl: 'https://api-dev.hunterkorea.com',
         timeout: 15000,
         tokenKeys: Object.freeze({
             access: 'hunter.accessToken',
